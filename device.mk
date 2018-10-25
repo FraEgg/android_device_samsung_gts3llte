3848c518e0417c7e8ce196f20a24d9b27ea7555b
Frank <eggen@emden.net>
2018-10-25 14:34:32 +0200

    Add AndroidBw Apps

--- device.mk
+++ device.mk
@@ -78,6 +78,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/qmi_config.xml
 
+# AndroidBw
+PRODUCT_PACKAGES += \
+    firefox \
+	signal \
+	fakegps
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
